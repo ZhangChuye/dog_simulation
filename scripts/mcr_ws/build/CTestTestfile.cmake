20aa3d9aeb4940079a6750cb5cbf1adb8a3835f2
Ye-2077 <12111813@mail.sustech.edu.cn>
2023-05-15 08:08:41 +0800

    the demo urdf of the robot

---
+++ scripts/mcr_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ye/Project/MCR/mcr_ws/src
+# Build directory: /home/ye/Project/MCR/mcr_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bot_description")
---
+++ scripts/mcr_ws/build/bot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ye/Project/MCR/mcr_ws/src/bot_description
+# Build directory: /home/ye/Project/MCR/mcr_ws/build/bot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
