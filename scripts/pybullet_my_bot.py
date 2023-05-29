c6969487265028f4f822a142c5cf6364912d0c3b
Ye-2077 <12111813@mail.sustech.edu.cn>
2023-05-29 16:37:28 +0800

    要提交的变更： 	修改：     scripts/pybullet_my_bot.py 	新文件：   urdf/bennett_bot.urdf

--- scripts/pybullet_my_bot.py
+++ scripts/pybullet_my_bot.py
@@ -5,7 +5,8 @@ import pybullet as p
 import pybullet_data
 
 client = p.connect(p.GUI)
-# client = pb.connect(pb.DIRECT) # non-graphical version
+#client = pb.connect(pb.DIRECT) # non-graphical version
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) # add search path for loadURDF.
 
 #pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 0) # disable rendering during creation.
@@ -26,15 +27,35 @@ for i in range(num_joints):
 	print(joint_info)
 	joint_infos.append(joint_info)
 
-maxforce = 10
-velocity = 0.4
-for i in range(len(joint_infos)):
-    if i == 0 or 3 or 6 or 9:
-        p.setJointMotorControl2(bodyUniqueId=robotID,
-                                       jointIndex=joint_infos[i][0],
-                                       controlMode=p.VELOCITY_CONTROL,
-                                       targetVelocity=velocity,
-                                       force=maxforce)
+# maxforce = 10
+# velocity = -0.4
+# for i in range(len(joint_infos)):
+#     if i == 0 or 3 or 6 or 9:
+#         p.setJointMotorControl2(bodyUniqueId=robotID,
+#                                        jointIndex=joint_infos[i][0],
+#                                        controlMode=p.VELOCITY_CONTROL,
+#                                        targetVelocity=velocity,
+#                                        force=maxforce)
+
+startposition = [0, 0, 0.2]
+robotStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+p.resetBasePositionAndOrientation(robotID, startposition, robotStartOrientation)
+
+# set original joint state
+p.resetJointState(robotID, 0, -1.57/2)
+p.resetJointState(robotID, 3, -1.57/2)
+p.resetJointState(robotID, 6, -1.57/2)
+p.resetJointState(robotID, 9, -1.57/2)
+
+p.resetJointState(robotID, 1, -1.57/4)
+p.resetJointState(robotID, 4, -1.57/4)
+p.resetJointState(robotID, 7, -1.57/4)
+p.resetJointState(robotID, 10, -1.57/4)
+
+p.resetJointState(robotID, 2, 1.57/2)
+p.resetJointState(robotID, 5, 1.57/2)
+p.resetJointState(robotID, 8, 1.57/2)
+p.resetJointState(robotID, 11, 1.57/2)
 
 while(True):
     p.stepSimulation()
